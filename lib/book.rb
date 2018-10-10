e4410fb74c4e474c966a37f5839d989d0a52f8c2
brianyu18 <brianyu1990@gmail.com>
2018-10-10 20:26:29 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book
+  def intialize (title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author
+    @author
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  
+  def genre
+    @genre
+  
+end
 
-
+Book.new("And Then There Were None")
